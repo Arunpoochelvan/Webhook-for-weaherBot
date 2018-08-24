2bc88cc8d233805778825d89b9217861754cf328
Arun Poochelvan <arunselvabio@gmail.com>
2018-08-24 13:09:47 +0530

    Add files via upload

--- webhook.py
+++ webhook.py
@@ -1,89 +1,47 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib as mpl
-import watson_developer_cloud as wdc
+import json
+import os
+import requests
+
 from flask import Flask
+from flask import request
+from flask import make_response
 
-
+# Flask app should start in global layout
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return max_tone_value
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    print(json.dumps(req, indent=4))
+    
+    res = makeResponse(req)
+    
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
-if __name__=="__main__":
-    app.run
- 
-tone_analyzer = wdc.ToneAnalyzerV3(
-  version='2016-05-19',
-  username='76776a39-f5a4-4ffc-a415-2d90626252a9',
-  password='5ILsVeprCpCE',
-  url= "https://gateway.watsonplatform.net/tone-analyzer/api",
-)
+def makeResponse(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    date = parameters.get("date")
+    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=a6400b9f821b0fc030dede972794b8b9')
+    json_object = r.json()
+    weather=json_object['list']
+    for i in range(0,30):
+        if date in weather[i]['dt_txt']:
+            condition= weather[i]['weather'][0]['description']
+            break
+    speech = "The forecast for"+city+"for "+date+" is "+condition
+    return {
+    "speech": speech,
+    "displayText": speech,
+    "source": "apiai-weather-webhook"
+    }
 
-
- 
-message = input("Input the context you want to test emotional analysis : ")
-
- 
-tone=tone_analyzer.tone(message, sentences=False, content_type='text/plain')
- 
-#assign each tone name and value to its respective category 
-emotion_tone={}
-language_tone={}
-social_tone={}
- 
-for cat in tone['document_tone']['tone_categories']:
-    print('Category:', cat['category_name'])
-    if cat['category_name'] == 'Emotion Tone':
-        for tone in cat['tones']:
-            print('-', tone['tone_name'], tone['score'])
-            emotion_tone.update({tone['tone_name']:tone['score']})     
-    if cat['category_name'] == 'Social Tone':
-        for tone in cat['tones']:
-            print('-', tone['tone_name'], tone['score'])
-            social_tone.update({tone['tone_name']:tone['score']}) 
-    if cat['category_name'] == 'Language Tone':
-        for tone in cat['tones']:
-            print('-', tone['tone_name'], tone['score'])
-            language_tone.update({tone['tone_name']:tone['score']})             
- 
- 
-#find largest value in all tones to adjust the x scale accordingly
-max_tone_value = {**emotion_tone, **language_tone, **social_tone}
-if max(max_tone_value.values()) > 0.9:
-    max_tone_value = 1
-else:
-    max_tone_value = max(max_tone_value.values())+0.1
- 
- 
-#plot all tones by category
-fig = plt.figure(figsize=(7,7))
-mpl.style.use('seaborn')
-fig.suptitle('Tones by Intensity, scale range: 0(min) - 1(max)', fontsize=14, fontweight='bold')
- 
-x1=fig.add_subplot(311)
-y_pos = np.arange(len(emotion_tone.keys()))
-plt.barh(y_pos, list(emotion_tone.values()), align='center', alpha=0.6, color='limegreen')
-plt.yticks(y_pos, emotion_tone.keys())
-plt.title('Emotion Tone', fontsize=12)
-x1.set_xlim([0, max_tone_value])
- 
-x2=fig.add_subplot(312)
-y_pos = np.arange(len(social_tone.keys()))
-plt.barh(y_pos, list(social_tone.values()), align='center', alpha=0.6,color='red')
-plt.yticks(y_pos, social_tone.keys())
-plt.title('Social Tone',fontsize=12)
-x2.set_xlim([0, max_tone_value])
- 
-x3=fig.add_subplot(313)
-y_pos = np.arange(len(language_tone.keys()))
-plt.barh(y_pos, list(language_tone.values()), height = 0.4, align='center', alpha=0.6, color='deepskyblue')
-plt.yticks(y_pos, language_tone.keys())
-plt.title('Language Tone',fontsize=12)
-x3.set_xlim([0, max_tone_value])
- 
-plt.tight_layout(pad=0.9, w_pad=0.5, h_pad=1.7)
-fig.subplots_adjust(top=0.85, left=0.20)
-plt.show()
-
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
